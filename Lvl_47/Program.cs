29a9149725fe8651d77b786122a9a9c0cdd72973
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-23 22:56:38 +0300

    Выполнена 47 задача

---
+++ Lvl_47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] CreateArray(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)        // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)    // 1- columns
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    Console.WriteLine("Сгенерированный массив:");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5} | ");
+            else Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2D = CreateArray(4, 3, -10, 10);
+
+PrintMatrix(array2D);
---
+++ Lvl_51/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)        // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)    // 1- columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine("Сгенерированный массив:");
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int LineMatrixMaker(int[,] matrix)
+{
+    int count = matrix.GetLength(0) > matrix.GetLength(1) ? 1 : 0;
+    int summM = default;
+    for (int i = 0; i < matrix.GetLength(count); i++)
+    {
+        summM += matrix[i, i];
+    }
+    return summM;
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 3, -10, 10);
+
+PrintMatrix(array2D);
+
+int _array2D = LineMatrixMaker(array2D);
+Console.WriteLine(_array2D);
